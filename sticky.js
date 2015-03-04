239b3c867bc2619263e8105d703c99b1deb73d7a
bdin <b.din1996@gmail.com>
2015-03-04 02:40:35 -0600

    Create sticky.js

---
+++ sticky.js
@@ -0,0 +1,14 @@
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#sticky').addClass('stick');
+    } else {
+        $('#sticky').removeClass('stick');
+    }
+}
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
